26a49f7368253338fa97ecb758b07b3aaffaf6d7
AFOP <felipe@ASUSAFOP.localdomain>
2023-04-03 22:24:04 -0500

    update task 0

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -9,23 +9,24 @@
  * @value: is the value to put in the new node
  * When created, a node does not have any children
  * Return: pointer to the new node, or NULL on failure
- */
-
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    if (parent != NULL)
-    {
-        if (value < parent->n)
-            parent->left = new_node;
-        else
-            parent->right = new_node;
-    }
-    return (new_node);
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	if (parent != NULL)
+	{
+		if (value < parent->n)
+			parent->left = new_node;
+		else
+			parent->right = new_node;
+	}
+	return (new_node);
 }
+
