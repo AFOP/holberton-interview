3e3b980a262fd3986d1ccd4d340c558c80d5aedb
AFOP <felipe@ASUSAFOP.localdomain>
2023-04-11 18:42:25 -0500

    Task 0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -22,5 +22,5 @@ def minOperations(n):
     for i in range(2, n+1):
         for j in range(1, i):
             if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i//j)
+                dp[i] = min(dp[i], dp[j] + i//j - 1)
     return dp[n]
