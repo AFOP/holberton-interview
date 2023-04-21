c162a0cc08331849f75991c9f606f2445250f205
AFOP <felipe@ASUSAFOP.localdomain>
2023-04-20 20:31:20 -0500

    task 0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -19,28 +19,6 @@ int is_stable(int grid1[3][3])
         {
             if (grid1[i][j] > 3)
             {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-/**
- * redistribute - redistribute the grid
- * @grid: 3x3 grid
- *
- */
-void redistribute(int grid1[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] > 3)
-            {
                 grid1[i][j] -= 4;
                 if (i > 0)
                     grid1[i - 1][j] += 1;
@@ -53,6 +31,7 @@ void redistribute(int grid1[3][3])
             }
         }
     }
+    return 1;
 }
 
 /**
@@ -65,23 +44,13 @@ void redistribute(int grid1[3][3])
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
     int i, j;
-    int temp[3][3];
 
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            temp[i][j] = grid1[i][j] + grid2[i][j];
+            grid1[i][j] += grid2[i][j];
         }
     }
-    while (!is_stable(temp)){
-        redistribute(temp);
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                grid1[i][j] = temp[i][j];
-            }
-        }
-    }
+    is_stable(grid1);
 }
