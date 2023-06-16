18ade9376aec9109be2675bb302122bc241ff785
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-15 20:23:04 -0500

    [UPDATE] README.md Task 0

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -11,62 +11,64 @@ def is_safe(board, row, col, N):
     Checking the row
     """
     for i in range(col):
-        if board[row][i] == 'Q':
+        if board[row][i] == 1:
             return False
 
     # Checking the upper diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 'Q':
+        if board[i][j] == 1:
             return False
 
     # Checking the lower diagonal
     for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 'Q':
+        if board[i][j] == 1:
             return False
 
     return True
 
 
-def solve_nqueens_util(board, col, N):
-    """Base case: If all queens are placed, print the solution"""
+def solve_nqueens_util(board, col, N, solutions):
+    """Base case: If all queens are placed, add the solution"""
     if col >= N:
-        print_solution(board, N)
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
         return True
 
     # Recursive case: Try placing the queen in each row of the current column
     for i in range(N):
         if is_safe(board, i, col, N):
-            board[i][col] = 'Q'
-            solve_nqueens_util(board, col + 1, N)
-            board[i][col] = '.'
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, N, solutions)
+            board[i][col] = 0
 
 
 def solve_nqueens(N):
-    """Create an empty N x N board"""
-    board = [['.' for _ in range(N)] for _ in range(N)]
-    solve_nqueens_util(board, 0, N)
+    """solve nqueens"""
+    if N < 4:
+        print(f"N must be at least 4")
+        sys.exit(1)
 
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_nqueens_util(board, 0, N, solutions)
 
-def print_solution(board, N):
-    """Print the board configuration"""
-    for i in range(N):
-        print(' '.join(board[i]))
-    print()
+    for solution in solutions:
+        print(solution)
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Usage: nqueens N')
+        print(f"Usage: nqueens N")
         sys.exit(1)
 
     try:
         N = int(sys.argv[1])
     except ValueError:
-        print('N must be a number')
-        sys.exit(1)
-
-    if N < 4:
-        print('N must be at least 4')
+        print(f"N must be a number")
         sys.exit(1)
 
     solve_nqueens(N)
