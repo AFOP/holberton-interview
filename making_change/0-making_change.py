9c510a30819bbcef51b202d2331815ea7e9a7827
AFOP <felipe@ASUSAFOP.localdomain>
2023-09-21 09:49:06 -0500

    [UPDATE] task 0

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+Given a pile of coins of different values, determine the fewest number 
+of coins needed to meet a given amount total.
 
 Prototype: def makeChange(coins, total)
 Return: fewest number of coins needed to meet total
@@ -8,14 +9,16 @@ If total is 0 or less, return 0
 If total cannot be met by any number of coins you have, return -1
 coins is a list of the values of the coins in your possession
 The value of a coin will always be an integer greater than 0
-You can assume you have an infinite number of each denomination of coin in the list
+You can assume you have an infinite number of each denomination
+of coin in the list
 Your solution’s runtime will be evaluated in this task
 """
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize a list to store the minimum number of coins needed for each total value from 1 to 'total'.
+    # Initialize a list to store the minimum 
+    # number of coins needed for each total value from 1 to 'total'.
     # Initialize all values to be a large number to begin with.
     dp = [float('inf')] * (total + 1)
     
@@ -27,6 +30,7 @@ def makeChange(coins, total):
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If dp[total] is still 'inf', it means it's not possible to make the total with the given coins.
+    # If dp[total] is still 'inf', it means it's not possible to make
+    # the total with the given coins.
     # Otherwise, return the minimum number of coins needed for the total.
     return dp[total] if dp[total] != float('inf') else -1
