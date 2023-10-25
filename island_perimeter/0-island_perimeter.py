9559264a969b445e50a81fde43ef708ada59807a
AFOP <felipe@ASUSAFOP.localdomain>
2023-10-25 09:50:55 -0500

    [UPDATE] task 0

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -2,25 +2,25 @@
 """
 Island Perimeter module
 """
-def island_perimeter(grid):
-    """ Calculate perimeter of the island (grid)
 
-        Args:
-            grid ([[int]]): is a list of list of integers
-        Return: perimeter of the island described in grid 
+
+def island_perimeter(grid):
+    """Calculate perimeter of the island (grid)
+
+    Args:
+        grid ([[int]]): is a list of list of integers
+    Return: perimeter of the island described in grid
     """
-    perimeter = 0
-    
-    # Define the possible neighboring directions
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                for dr, dc in directions:
-                    new_row, new_col = row + dr, col + dc
-                    # Check if the neighbor is out of bounds or is water
-                    if new_row < 0 or new_row >= len(grid) or new_col < 0 or new_col >= len(grid[0]) or grid[new_row][new_col] == 0:
-                        perimeter += 1
-    
-    return perimeter
+    _sum, limit = (0, 0)
+    len_grid, len_col = (len(grid), len(grid[0]))
+
+    for col in range(len_grid):
+        _sum += sum(grid[col])
+        for row in range(len_col):
+            if grid[col][row]:
+                if row > 0 and grid[col][row - 1] == 1:
+                    limit += 1
+
+                if col > 0 and grid[col - 1][row] == 1:
+                    limit += 1
+    return _sum * 4 - limit * 2
